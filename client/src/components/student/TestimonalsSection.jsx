79353defb29707fc2639e9e4cd44203435761a76
Nagari Pavan <pawanmbnr03@gmail.com>
2026-02-09 19:02:14 +0530

    initial commit

---
+++ client/src/components/student/TestimonalsSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TestimonalsSection = () => {
+  return (
+    <div>
+        <h1>Testimonals Section</h1>
+    </div>
+  )
+}
+
+export default TestimonalsSection
