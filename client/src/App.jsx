e917d5b99bed40f36035b65b755d18c260186912
Nagari Pavan <pawanmbnr03@gmail.com>
2026-02-09 19:36:34 +0530

    Created Routes for Student and Educator

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/student/Home'
+import CoursesList from './pages/student/CoursesList'
+import CourseDetails from './pages/student/CourseDetails'
+import MyEnrollments from './pages/student/MyEnrollments'
+import Player from './pages/student/Player'
+import Loading from './components/student/Loading'
+import Educator from './pages/educator/Educator'
+import AddCourse from './pages/educator/AddCourse'
+import MyCourses from './pages/educator/MyCourses'
+import StudentsEnrolled from './pages/educator/StudentsEnrolled'
+import Dashboard from './pages/educator/Dashboard'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        {/* Routes for Student */}
+        <Route path='/' element={<Home/>}/>
+        <Route path='/course-list' element={<CoursesList/>}/>
+        <Route path='/course-list/:input' element={<CoursesList/>}/>
+        <Route path='/course/:id' element={<CourseDetails/>}/>
+        <Route path='/my-enrollments' element={<MyEnrollments/>}/>
+        <Route path='/player/:courseId' element={<Player/>}/>
+        <Route path='/loading/:path' element={<Loading/>}/>
+
+        {/* Routes for Educator */}
+        <Route path='/educator' element={<Educator/>}>
+            <Route path='educator' element={<Dashboard/>}/>
+            <Route path='add-course' element={<AddCourse/>}/>
+            <Route path='my-courses' element={<MyCourses/>}/>
+            <Route path='students-enrolled' element={<StudentsEnrolled/>}/>
+        </Route>
+        
+      </Routes>
+    </div>
   )
 }
 
--- client/src/pages/educator/Educator.jsx
+++ client/src/pages/educator/Educator.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 const Educator = () => {
   return (
     <div>
         <h1>Educator Page</h1>
+        <div>
+            <Outlet />
+        </div>
+
     </div>
   )
 }
