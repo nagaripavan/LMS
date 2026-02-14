6db5fa28ef596b9cc29532af62af573a44962d71
Nagari Pavan <pawanmbnr03@gmail.com>
2026-02-14 18:08:24 +0530

    feat: add search box component with functionality

--- client/src/components/student/Hero.jsx
+++ client/src/components/student/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { assets } from '../../assets/assets'
+import SearchBar from './SearchBar'
 
 const Hero = () => {
   return (
@@ -12,6 +13,8 @@ const Hero = () => {
 
         <p className='md:block hidden text-gray-500 max-w-2xl mx-auto'>We bring together world-class instructors, interactive content, and a supportive community to help you achieve your personal and professional goals.</p>
         <p className='md:hidden text-gray-500 max-w-sm max-auto'>We bring together world-class instructors to help you achieve your professional goals.</p>
+
+        <SearchBar />
     </div>
   )
 }
--- client/src/components/student/SearchBar.jsx
+++ client/src/components/student/SearchBar.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { assets } from '../../assets/assets'
+import { useNavigate } from 'react-router-dom'
 
-const SearchBar = () => {
+const SearchBar = ({data}) => {
+
+  const navigate = useNavigate()
+  const [input,setInput] = useState(data?data:'')
+
+  const onSearchHandler = (e) => {
+    e.preventDefault()
+    navigate('/course-list/'+input)
+  }
+
   return (
-    <div>
-        <h1>SearchBar</h1>
-    </div>
+    <form onSubmit={onSearchHandler} className='flex items-center border border-gray-300 rounded-md px-1 py-1  w-full md:w-auto space-x-3'>
+        <img className='pl-3' src={assets.search_icon} alt="search" />
+        <input onChange={e=>setInput(e.target.value)} value={input} type="text" placeholder='Search for courses...' className='w-full md:w-96 px-4 py-2 outline-none'/>
+        <button className='bg-blue-600 px-7 py-2 rounded text-white font-light'>Search</button>
+    </form>
   )
 }
 
