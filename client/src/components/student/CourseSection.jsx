d66bcc95146564022679eb9f3c269da5981b3d4a
Nagari Pavan <pawanmbnr03@gmail.com>
2026-02-15 12:40:51 +0530

    chore: downgrade tailwind to v3 and fix config setup

--- client/src/components/student/CourseSection.jsx
+++ client/src/components/student/CourseSection.jsx
@@ -11,9 +11,9 @@ const CourseSection = () => {
   return (
     <div className='pt-15 md:px-30 px-5'>
         <h2 className='text-3xl font-semibold text-gray-800'>Learn from the best</h2>
-        <p className='pt-3 text-gray-500 text-sm'>Discover our top-rated courses across various categories. From coding and design to business and wellness, our courses are crafted to deliver results.</p>
+        <p className='pt-3 text-gray-500 text-sm'>Discover our top-rated courses across various categories. From coding and design to <br /> business and wellness, our courses are crafted to deliver results.</p>
         
-        <div className='grid md:grid-cols-4 px-4 md:px-0 md:mt-16 my-10 gap-4'>
+        <div className='grid grid-cols-auto px-4 md:px-0 md:mt-16 my-10 gap-4'>
           {allCourses.slice(0,4).map((course,index)=><CourseCard key={index} course={course} />)}
         </div>
 
