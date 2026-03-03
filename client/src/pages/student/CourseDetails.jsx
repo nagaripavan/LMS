c46d01634c5dc2a36e9c45b5db3e1b9131583c7a
Nagari Pavan <pawanmbnr03@gmail.com>
2026-03-03 17:45:29 +0530

    feat: add animated spinner to Loading component

--- client/src/pages/student/CourseDetails.jsx
+++ client/src/pages/student/CourseDetails.jsx
@@ -5,6 +5,8 @@ import Loading from '../../components/student/Loading'
 import { assets } from '../../assets/assets'
 import humanizeDuration from 'humanize-duration'
 import Footer from '../../components/student/Footer'
+import YouTube from 'react-youtube'
+
 
 const CourseDetails = () => {
 
@@ -13,6 +15,7 @@ const CourseDetails = () => {
   const [courseData,setCourseData] = useState(null)
   const [openSections,setOpenSections] = useState({})
   const [isAlreadyEnrolled,setIsAlreadyEnrolled] = useState(false)
+  const [playerData,setPlayerData] = useState(null)
 
   const {allCourses,calculateRating, calculateChapterTime,
     calculateCourseDuration,calculateNoOfLectures,currency} = useContext(AppContext)
@@ -86,7 +89,10 @@ const CourseDetails = () => {
                             <div className='flex items-center justify-between w-full text-gray-800 text-xs md:text-default'>
                               <p>{lecture.lectureTitle}</p>
                               <div className='flex gap-2'>
-                                {lecture.isPreviewFree && <p className='text-blue-500 cursor-pointer'>Preview</p>}
+                                {lecture.isPreviewFree && <p onClick={()=>setPlayerData({
+                                  videoId: lecture.lectureUrl.split('/').pop()
+                                })}
+                                 className='text-blue-500 cursor-pointer'>Preview</p>}
                                 <p>{humanizeDuration(lecture.lectureDuration * 60 * 1000, {units:['h','m']})}</p>
                               </div>
                             </div>
@@ -111,7 +117,14 @@ const CourseDetails = () => {
         {/* right cloumn*/}
         <div className='max-w-course-card z-10 shadow-custom-card rounded-t
         md:rounded-none overflow-hidden bg-white min-w-[300px] sm:min-w-[420px] '>
-          <img src={courseData.courseThumbnail} alt="Thumbnail" />
+         
+         {
+          playerData?
+          <YouTube videoId={playerData.videoId} opts={{playerVars:{autoplay:1}}} iframeClassName='w-full aspect-video'/>
+          :<img src={courseData.courseThumbnail} alt="Thumbnail" />
+
+         }
+
           <div className='p-5'>
             <div className='flex items-center gap-2'>
               <img className='w-3.5' src={assets.time_left_clock_icon} alt="time" />
