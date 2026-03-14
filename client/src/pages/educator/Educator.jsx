b460c7b53d8414f33344693b6c2e736d491b3288
Nagari Pavan <pawanmbnr03@gmail.com>
2026-03-14 18:29:39 +0530

    Designed Footer component for Educator page

--- client/src/pages/educator/Educator.jsx
+++ client/src/pages/educator/Educator.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from '../../components/educator/Navbar'
 import SideBar from '../../components/educator/SideBar'
+import Footer from '../../components/educator/Footer'
 
 const Educator = () => {
   return (
@@ -14,7 +15,7 @@ const Educator = () => {
             </div>
             
         </div>
-
+      <Footer/>
     </div>
   )
 }
